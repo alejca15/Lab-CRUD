ce4f249684965a3950e1643ffef038549c5640f0
BaltodanoDalhuin <dbaltodano@fwdcostarica.com>
2024-10-11 09:57:27 -0600

    Avance1

--- Src/Products_Routes.js
+++ Src/Products_Routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Products_Routes = require('../Src/Controllers/Product_Controllers')
 
-router.get('/', productosController.getAllProductos)
-
+router.get('/', Products_Routes.getAllProducts)
 
 module.exports=router
