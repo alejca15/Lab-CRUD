c2b2a4ca08b4984b7e859b0e37a0ef50a3c9d512
acarmfwd <acarmona@fwdcostarica.com>
2024-10-11 14:59:19 -0600

    Se termino el proyecto

--- Src/Routes/Products_Routes.js
+++ Src/Routes/Products_Routes.js
@@ -4,6 +4,8 @@ const Products_Routes = require('../Controllers/Product_Controllers')
 
 router.get('/', Products_Routes.getAllProducts)
 router.post(`/`,Products_Routes.post_product )
+router.get('/:id', Products_Routes.get_id_product)
 router.put(`/:id`,Products_Routes.updateProduct)
+router.delete(`/:id`,Products_Routes.delete_product)
 
 module.exports=router
